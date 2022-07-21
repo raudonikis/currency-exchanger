9f02aefbaf6b2c2a1a50440a8df7876355cc01b5
raudonikis <raudonisernestas@gmail.com>
2022-07-21 13:41:44 +0300

    Rename the project from the template

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,4 +6,4 @@ include(":libraries:data")
 include(":libraries:common")
 include(":libraries:network")
 include(":app")
-rootProject.name = "Android Skeleton"
+rootProject.name = "Currency exchanger"
