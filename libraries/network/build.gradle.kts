bb6b56c049a2272314393485efe2a6bfeccddd18
raudonikis <raudonisernestas@gmail.com>
2022-07-26 19:42:55 +0300

    Extract api key into local.properties

--- libraries/network/build.gradle.kts
+++ libraries/network/build.gradle.kts
@@ -1,8 +1,25 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.library")
     id("dagger.hilt.android.plugin")
     kotlin("android")
     kotlin("kapt")
+}
+
+val localProperties = Properties()
+val localPropertiesFile = project.rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(FileInputStream(localPropertiesFile))
+}
+
+android {
+    namespace = "com.raudonikis.network"
+    defaultConfig {
+        val fixerApiKey = "FIXER_API_KEY"
+        buildConfigField("String", fixerApiKey, localProperties.getProperty(fixerApiKey))
+    }
 }
 
 dependencies {
@@ -16,7 +33,4 @@ dependencies {
     // DI
     implementation(Dependencies.daggerHilt)
     kapt(Dependencies.daggerCompiler)
-}
-android {
-    namespace = "com.raudonikis.network"
 }
--- libraries/network/src/main/java/com/raudonikis/network/FixerApiKeys.kt
+++ libraries/network/src/main/java/com/raudonikis/network/FixerApiKeys.kt
@@ -1,3 +1,4 @@
 package com.raudonikis.network
 
 const val FIXER_API_BASE_URL = "https://api.apilayer.com/fixer/"
+const val FIXER_API_KEY = BuildConfig.FIXER_API_KEY
